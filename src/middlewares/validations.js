47dce79561ad1f2b7e0a605a68947c4548dfc1a2
Yanni <yannidilly.prog@gmail.com>
2023-02-13 23:29:11 -0300

    corrige erro na escrita da constante que salva o token

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -22,7 +22,7 @@ const passwordValidation = (req, res, next) => {
 };
 
 const tokenValidation = (req, res, next) => {
-  const token = req.header.authorization;
+  const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
