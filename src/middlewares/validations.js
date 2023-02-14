775106fc96bc56146282b74d677fe60f3e51c57b
Yanni <yannidilly.prog@gmail.com>
2023-02-14 11:54:50 -0300

    altera forma de validação do campo rate

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -82,10 +82,10 @@ const watchedAtValidation = (req, res, next) => {
 
 const rateValidation = (req, res, next) => {
   const { rate } = req.body.talk;
-  if (!rate) {
+  if (rate === undefined) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
-  if (rate > 0 || rate < 5) {
+  if (rate > 0 || rate <= 5 || !Number.isInteger(rate)) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
   next();
