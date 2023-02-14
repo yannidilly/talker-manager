a3bbc8a2104b0f05960c2b1c16d3027101bde43e
Yanni <yannidilly.prog@gmail.com>
2023-02-13 22:46:34 -0300

    renomeia variaveis para evitar conflitos

--- src/index.js
+++ src/index.js
@@ -10,37 +10,37 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/', (req, res) => {
+  res.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
 
-app.get('/talker/:id', async (request, response) => {
+app.get('/talker/:id', async (request, res) => {
   const { id } = request.params;
   const talkersData = await readTalkers();
   const talkerRequestData = talkersData.find((talkerData) => talkerData.id === Number(id));
   if (!talkerRequestData) {
-    return response.status(404).json({
+    return res.status(404).json({
       message: 'Pessoa palestrante não encontrada',
     });
   }
-  return response.status(HTTP_OK_STATUS).json(talkerRequestData);
+  return res.status(HTTP_OK_STATUS).json(talkerRequestData);
 });
 
-app.get('/talker', async (_request, response) => {
+app.get('/talker', async (req, res) => {
   const talkersData = await readTalkers();
-  response.status(HTTP_OK_STATUS).json(talkersData);
+  res.status(HTTP_OK_STATUS).json(talkersData);
 });
 
-app.post('/login', emailValidation, passwordValidation, (request, response) => (
-  response.status(200).json({
+app.post('/login', emailValidation, passwordValidation, (request, res) => (
+  res.status(200).json({
     token: crypto.randomBytes(8).toString('hex'),
 })));
 
-app.post('/talker', (request, response) => {
+app.post('/talker', (request, res) => {
   // const { name, age, talk: { watchedAt, rate } } = request.body;
-  response.status(201).json(request.body);
+  res.status(201).json(request.body);
 });
